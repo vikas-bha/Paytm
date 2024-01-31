bc42ad17428bdfe33e414c1b2fd77857c818ecd9
Vikas Bhardwaj <vikasbhardwaj@Vikass-MacBook-Air.local>
2024-01-31 15:43:36 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
-const express = require("express");
+// backend/index.js
+const express = require('express');
+const cors = require("cors");
+const rootRouter = require("./routes/index");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", rootRouter);
+
+app.listen(3000, ()=>{
+    console.log("server has started");
+});
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+   <>
+          <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} /> */}
+        </Routes>
+      </Router>
+
+   </>
   )
 }
 
